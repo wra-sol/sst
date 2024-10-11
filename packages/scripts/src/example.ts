f23c5e13bd31253a34bbdad425c4b65b72ecbf1a
Nathaniel Arfin <info@nathanielarfin.com>
2024-10-11 16:07:57 -0400

    Initial commit

--- packages/scripts/src/example.ts
+++ packages/scripts/src/example.ts
@@ -1,4 +1,4 @@
 import { Resource } from "sst";
-import { Example } from "@monorepo-template/core/example";
+import { Example } from "@sst/core/example";
 
 console.log(`${Example.hello()} Linked to ${Resource.MyBucket.name}.`);
